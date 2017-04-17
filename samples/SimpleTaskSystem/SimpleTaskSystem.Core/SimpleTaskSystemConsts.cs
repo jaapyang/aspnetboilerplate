73df824a123171020d7439b6b10f13629ee18808
jaapyang <15873115257@qq.com>
2017-04-17 23:57:56 +0800

    no comment

--- samples/SimpleTaskSystem/SimpleTaskSystem.Core/SimpleTaskSystemConsts.cs
+++ samples/SimpleTaskSystem/SimpleTaskSystem.Core/SimpleTaskSystemConsts.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleTaskSystem.Core
+﻿namespace SimpleTaskSystem.Core
 {
-    class Class1
+    public class SimpleTaskSystemConsts
     {
+        public const string LocalizationSourceName = "SimpleTaskSystem";
     }
 }
